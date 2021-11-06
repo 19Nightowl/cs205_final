f86a1f1da92ce7a917f3eeb91a4860099724ef2f
Robin Pace <robin.pace@gmail.com>
2021-11-05 17:55:38 -0700

    Edited a file to read certain columns of data and return the average

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -5,7 +5,28 @@
 #    Avg. Attack: [VALUE]
 # ======= END SUMMARY =======
 
+BEGIN{FS="\t"}
+{
+  if (NR!=1){
+	# Change HP to add up the number of HP on the Pokemon listed
+		hp+=$6;
+	# Change attack to add up the number of attack on the Pokemon listed
+		attack+=$7;
+
+  }
+  #Counts the number of pokemon in the file
+  numOfPokemon=NR-1;
+}
+END{
+  # Prints the number of lines in the file
+  print "Total Pokemon: ", numOfPokemon
+  # Prints the average hp
+  print "Avg. Hp: ", hp/numOfPokemon
+  # Prints the average attack
+  print "Avg. Attack: ", attack/numOfPokemon}
+
 # The "Avg." values should be calculated as mean values for the corresponding columns.
+
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
